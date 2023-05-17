d007f1efb7a9a8a1d840fa2a152f820e2cbfeab0
Jirayut Nimsaeng (Dear) <winggundamth@users.noreply.github.com>
2023-05-17 11:34:08 +0700

    Fix indent

--- src/ratings/ratings.test.js
+++ src/ratings/ratings.test.js
@@ -1,16 +1,16 @@
 test('adds 1 + 1 to equal 2', () => {
-    expect(1 + 1).toBe(2);
-  });
-  
-  test('adds 1 + 2 to equal 3', () => {
-    expect(1 + 2).toBe(3);
-  });
-  
-  test('adds 2 + 1 to equal 3', () => {
-    expect(2 + 1).toBe(3);
-  });
-  
-  test('adds 2 + 2 to equal 4', () => {
-    expect(2 + 2).toBe(4);
-  });
+  expect(1 + 1).toBe(2);
+});
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(1 + 2).toBe(3);
+});
+
+test('adds 2 + 1 to equal 3', () => {
+  expect(2 + 1).toBe(3);
+});
+
+test('adds 2 + 2 to equal 4', () => {
+  expect(2 + 2).toBe(4);
+});
   
